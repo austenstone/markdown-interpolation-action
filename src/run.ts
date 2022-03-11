8cab5b28df5739eb9733e42af8042fd2df6b4ef0
Austen Stone <austenstone@github.com>
2022-03-10 19:45:20 -0500

    test

--- src/run.ts
+++ src/run.ts
@@ -2,8 +2,7 @@ import * as core from '@actions/core';
 import { markdownInterpolateFileWrite } from 'markdown-interpolation';
 
 const run = async (): Promise<void> => {
-  const valuesInput = '{"TIME":"6:20 PM"}';
-  // const valuesInput = core.getInput('values');
+  const valuesInput = core.getInput('values');
   if (!valuesInput) return core.setFailed('No input \'values\'');
   core.info(valuesInput);
   let values;
